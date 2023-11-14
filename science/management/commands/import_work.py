567ff9994371e98b5f1bf1eb5ec1c9f3556fe3ac
elaikona <zwj1813@163.com>
2023-11-14 22:04:34 +0800

    create Work

--- science/management/commands/import_work.py
+++ science/management/commands/import_work.py
@@ -5,6 +5,7 @@
 # @FileName: import_work.py
 ===========================
 """
+import requests
 from django.core.management.base import BaseCommand
 
 
@@ -12,4 +13,8 @@ class Command(BaseCommand):
     help = 'to import work data from openalex'
 
     def handle(self, *args, **kwargs):
+        print("---test---")
+        # api = "https://api.openalex.org/works?per-page=50&page=1&filter=cited_by_count:%3E1000"
+        # response = requests.get(api)
+        # print(response.json())
         pass
