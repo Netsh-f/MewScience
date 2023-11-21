131cda10d8c79e2d638e4ff20e18fb6d75cdadc0
elaikona <zwj1813@163.com>
2023-11-21 19:05:07 +0800

    create Work and others

--- science/management/commands/import_work.py
+++ science/management/commands/import_work.py
@@ -8,13 +8,17 @@
 import requests
 from django.core.management.base import BaseCommand
 
+from science.models import Work
+
 
 class Command(BaseCommand):
     help = 'to import work data from openalex'
 
     def handle(self, *args, **kwargs):
-        print("---test---")
-        # api = "https://api.openalex.org/works?per-page=50&page=1&filter=cited_by_count:%3E1000"
-        # response = requests.get(api)
-        # print(response.json())
-        pass
+        for i in range(2, 4):
+            api = "https://api.openalex.org/works?per-page=100&page=" + str(i) + "&filter=concept.id:C41008148"
+            response = requests.get(api).json()
+            results = response['results']
+            for result in results:
+                Work.objects.create(data=result)
+            print(f"page {i} import finished")
--- science/models.py
+++ science/models.py
@@ -3,5 +3,21 @@ from django.db import models
 
 # Create your models here.
 
-class Work(models.Model):
+class Works(models.Model):
+    data = models.JSONField()
+
+
+class Authors(models.Model):
+    data = models.JSONField()
+
+
+class Sources(models.Model):
+    data = models.JSONField()
+
+
+class Institutions(models.Model):
+    data = models.JSONField()
+
+
+class Concepts(models.Model):
     data = models.JSONField()
