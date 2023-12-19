04bb35795a681ea0cdfff0fa8fc0e7ba79ba301a
elaikona <zwj1813@163.com>
2023-12-19 20:06:02 +0800

    add userprofile

--- account/models.py
+++ account/models.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
 
+class UserProfile(models.Model):
+    class Identify(models.IntegerChoices):
+        NORMAL = 0,
+        ADMIN = 1,
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    identity = models.PositiveSmallIntegerField(choices=Identify.choices, default=Identify.NORMAL)
+    researcher_id = models.BigIntegerField(null=True)
