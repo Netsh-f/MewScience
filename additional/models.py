7926023c4ab2d9dc616fe6e5c8db1110cc525c67
BigBig_Zhe <1696144659@qq.com>
2023-12-22 19:17:44 +0800

    add script for adding project

--- additional/models.py
+++ additional/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Project(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=64)
-    application_code = models.CharField(max_length=8)
+    title = models.CharField(max_length=128)
+    application_code = models.CharField(max_length=10)
     authors = models.JSONField()
-    supporting_units = models.CharField(max_length=64)
+    supporting_units = models.CharField(max_length=128)
     funds = models.FloatField()
-    abstract = models.CharField(max_length=1024)
-    url = models.CharField(max_length=128)
+    abstract_c = models.BinaryField()
+    abstract_e = models.BinaryField()
 
 
 class Patent(models.Model):
