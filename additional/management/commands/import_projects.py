2e25e061c9105abe1db26165dab04ecdaff39fc8
BigBig_Zhe <1696144659@qq.com>
2023-12-23 11:38:58 +0800

    change project model

--- additional/management/commands/import_projects.py
+++ additional/management/commands/import_projects.py
@@ -10,14 +10,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data = []
+        project_authors = {}
+        with open("D:\\author", "r", encoding="utf-8") as f:
+            for line in f:
+                j = json.loads(line)
+                for key in j.keys():
+                    project_authors[key] = [int(i) for i in j[key]]
         with open("D:\\output1.txt", "r", encoding="utf-8") as f:
             while True:
                 line = f.readline()
                 if len(line) == 0:
                     break
                 j = json.loads(line)
+                authors = ""
+                for par in j["participatantsList"]:
+                    authors += par['result'][1] + ";"
                 data.append(Project(id=int(j["ratifyNo"]), title=j["projectName"], application_code=j["code"],
-                                    authors=json.loads('{}'), supporting_units=j["dependUnit"], funds=float(j["supportNum"]),
+                                    authors=authors,
+                                    authors_r=json.dumps(project_authors[j["ratifyNo"]]), supporting_units=j["dependUnit"], funds=float(j["supportNum"]),
                                     abstract_c=j['projectAbstractC'],
                                     abstract_e=j['projectAbstractE']))
         Project.objects.bulk_create(data)
