5824bd895696aaec8eeeba1bcfddeb78ea5e19d4
elaikona <zwj1813@163.com>
2023-12-24 16:54:06 +0800

    add get source

--- science/views/sources.py
+++ science/views/sources.py
@@ -10,10 +10,18 @@ import copy
 from rest_framework.decorators import api_view
 
 from MewScience.settings import ES
-from science.request_serializers import GetHotSerializer
+from science.request_serializers import GetHotSerializer, IdSerializer
 from utils.decorators import validate_request
 from utils.error_code import ErrorCode
 from utils.response_util import api_response
+
+
+@api_view(['GET'])
+@validate_request(IdSerializer)
+def get_source(request, serializer):
+    id = serializer.validated_data.get('id')
+    result = ES.get(index='sources', id=id)
+    return api_response(ErrorCode.SUCCESS, result['_source'])
 
 
 @api_view(['GET'])
